eb3b5f85955c32caa57424334ebb52c038c6f1ee
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-05 22:35:50 +0100

    feat(spinner): :sparkles: add loading spinner component

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full text-center flex flex-1 justify-center items-center py-6">
+    <span class="loading loading-spinner text-secondary loading-lg"></span>
+  </div>
+</template>
