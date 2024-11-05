e2aafc7e223e4c857ea78e8d4555bd9aaf825522
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-05 22:01:35 +0100

    feat(mythic-profile-list-item): :sparkles: add hover and shadow

--- src/components/MythicKeystoneProfileListItem.vue
+++ src/components/MythicKeystoneProfileListItem.vue
@@ -7,18 +7,23 @@ defineProps<{
 </script>
 
 <template>
-  <div class="text-center shadow rounded-lg bg-accent-content p-6">
-    <div>
-      <div class="avatar online">
-        <div class="w-16 rounded-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-          />
-        </div>
-      </div>
+  <div
+    class="text-center rounded-lg bg-accent-content p-6 relative shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg group overflow-hidden"
+  >
+    <div class="w-full mx-auto h-auto overflow-hidden rounded-lg float-left">
+      <img
+        src="@/assets/dummy.jpg"
+        :alt="dungeon.name"
+        class="w-full h-auto z-0 transition-all duration-300 group-hover:scale-110 absolute top-0 left-0"
+      />
+      <div
+        class="absolute inset-0 bg-gradient-to-t from-black via-gray-900 to-black opacity-85"
+      ></div>
     </div>
-    <div class="text-primary">{{ dungeon.name }}</div>
-    <div class="text-secondary text-3xl font-bold">{{ dungeon.score }}</div>
-    <div class="">Best run: 25:14</div>
+    <div class="relative flex flex-col gap-2">
+      <div class="text-primary font-bold">{{ dungeon.name }}</div>
+      <div class="text-secondary text-3xl font-bold">{{ dungeon.score }}</div>
+      <div class="">Best run: 25:14</div>
+    </div>
   </div>
 </template>
