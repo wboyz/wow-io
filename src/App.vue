51358de0beacf77dbc877175cf97994e7802f308
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-05 22:41:18 +0100

    fix(layout): :lipstick: adjust app layout height
    - add h-screen
    - fix tabindex ul issue
    - fix button role issue

--- src/App.vue
+++ src/App.vue
@@ -3,47 +3,50 @@ import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>
-    <div class="navbar bg-base-100">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+  <div class="h-screen flex flex-col">
+    <header>
+      <div class="navbar bg-base-100">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <button tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </button>
+            <ul
+              class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <li><RouterLink to="/">Home</RouterLink></li>
+              <li>
+                <RouterLink to="/mythic-profile">Mythic Profile</RouterLink>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabindex="0"
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
+          <a class="btn btn-ghost text-xl">WoW IO</a>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal px-1">
             <li><RouterLink to="/">Home</RouterLink></li>
             <li>
               <RouterLink to="/mythic-profile">Mythic Profile</RouterLink>
             </li>
           </ul>
         </div>
-        <a class="btn btn-ghost text-xl">WoW IO</a>
+        <div class="navbar-end"></div>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal px-1">
-          <li><RouterLink to="/">Home</RouterLink></li>
-          <li><RouterLink to="/mythic-profile">Mythic Profile</RouterLink></li>
-        </ul>
-      </div>
-      <div class="navbar-end"></div>
-    </div>
-  </header>
+    </header>
 
-  <RouterView />
+    <RouterView />
+  </div>
 </template>
