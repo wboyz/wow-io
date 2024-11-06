c85464366554a2a6741eea9f3b62dafac6ed90cc
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-07 00:04:28 +0100

    feat(mythic-list): :sparkles: use battlenet api

--- src/views/MythicProfileView.vue
+++ src/views/MythicProfileView.vue
@@ -3,68 +3,30 @@ import { ref } from 'vue';
 import LoadingSpinner from '@/components/LoadingSpinner.vue';
 import MythicProfileForm from '@/components/MythicKeystoneProfileForm.vue';
 import MythicProfileList from '@/components/MythicKeystoneProfileList.vue';
-import type { MythicDungeon } from '@/types/MythicDungeon';
+import { useBattleNetStore } from '@/stores/battlenet';
+import type { Run } from '@/types/MythicKeystoneProfileSeason/Run';
+import { storeToRefs } from 'pinia';
 
-const dungeons: Array<MythicDungeon> = [
-  {
-    id: 376,
-    name: 'The Necrotic Wake',
-    score: 110,
-    keyNumber: 4,
-  },
-  {
-    id: 353,
-    name: 'Siege of Boralus',
-    score: 200,
-    keyNumber: 4,
-  },
-  {
-    id: 505,
-    name: 'The Dawnbreaker',
-    score: 300,
-    keyNumber: 4,
-  },
-  {
-    id: 507,
-    name: 'Grim Batol',
-    score: 200,
-    keyNumber: 3,
-  },
-  {
-    id: 501,
-    name: 'The Stonevault',
-    score: 150,
-    keyNumber: 5,
-  },
-  {
-    id: 502,
-    name: 'City of Threads',
-    score: 101,
-    keyNumber: 8,
-  },
-  {
-    id: 503,
-    name: 'Ara-Kara, City of Echoes',
-    score: 200,
-    keyNumber: 74,
-  },
-  {
-    id: 375,
-    name: 'Mists of Tirna Scithe',
-    score: 234,
-    keyNumber: 6,
-  },
-];
+const battleNetStore = useBattleNetStore();
+const { loading } = storeToRefs(battleNetStore);
+const runs = ref<Run[]>([]);
 
-const loaded = ref(false);
+async function search(realm: string, character: string) {
+  const response = await battleNetStore.mythicKeystoneProfileSeason(
+    realm,
+    character,
+  );
+
+  runs.value = response.best_runs;
+}
 </script>
 
 <template>
   <main class="lg:container mx-auto px-6 mt-6 flex flex-1 flex-col w-full">
-    <MythicProfileForm />
+    <MythicProfileForm @submit="search" />
 
-    <LoadingSpinner v-if="!loaded" />
+    <LoadingSpinner v-if="loading" />
 
-    <MythicProfileList :dungeons="dungeons" v-if="loaded" />
+    <MythicProfileList :runs="runs" v-if="!loading" />
   </main>
 </template>
