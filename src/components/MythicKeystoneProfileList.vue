15a7ee8d0c7574bd65475fc4ec1b394db6277d77
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-07 00:11:28 +0100

    feat(mythic-list): :sparkles: adjust gap

--- src/components/MythicKeystoneProfileList.vue
+++ src/components/MythicKeystoneProfileList.vue
@@ -8,7 +8,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="mt-6 grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+  <div class="mt-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
     <MythicKeystoneProfileListItem
       v-for="(run, index) in runs"
       :run="run"
