56539dba0612ecfc48dd7867cf3f9b2d7bd945a5
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-07 00:03:09 +0100

    fix(battlenet): :bug: inject
    add loading

--- src/stores/battlenet.ts
+++ src/stores/battlenet.ts
@@ -1,16 +1,20 @@
 import type { BattleNetClient } from '@/services/BattleNetClient';
 import { defineStore } from 'pinia';
-import { inject } from 'vue';
+import { inject, ref } from 'vue';
 
 export const useBattleNetStore = defineStore('battlenet', () => {
+  const loading = ref(false);
+  const battleNetClient = inject('battleNetClient') as BattleNetClient;
+
   async function mythicKeystoneProfileSeason(realm: string, character: string) {
-    const battleNetClient = inject('battleNetClient') as BattleNetClient;
+    loading.value = true;
     const response = await battleNetClient.fetchMythicKeystoneProfileSeason(
       realm,
       character,
     );
+    loading.value = false;
     return response;
   }
 
-  return { mythicKeystoneProfileSeason };
+  return { mythicKeystoneProfileSeason, loading };
 });
