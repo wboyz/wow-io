e7eb52d56ef7b4c6fe579df297e77f99d33a68fa
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-06 23:17:26 +0100

    feat(battlenet): :sparkles: rename store

--- src/stores/battlenet.ts
+++ src/stores/battlenet.ts
@@ -2,7 +2,7 @@ import type { BattleNetClient } from '@/services/BattleNetClient';
 import { defineStore } from 'pinia';
 import { inject } from 'vue';
 
-export const useBattlenetStore = defineStore('battlenet', () => {
+export const useBattleNetStore = defineStore('battlenet', () => {
   async function mythicKeystoneProfileSeason(realm: string, character: string) {
     const battleNetClient = inject('battleNetClient') as BattleNetClient;
     const response = await battleNetClient.fetchMythicKeystoneProfileSeason(
