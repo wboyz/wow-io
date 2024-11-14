d7454483b204458e30ad259e43dd01858f70355c
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-14 22:40:22 +0100

    fix(battlenet): :bug: handle errors in mythicKeystoneProfileSeason function

--- src/stores/battlenet.ts
+++ src/stores/battlenet.ts
@@ -4,17 +4,33 @@ import { inject, ref } from 'vue';
 
 export const useBattleNetStore = defineStore('battlenet', () => {
   const loading = ref(false);
+  const error = ref<string | null>(null);
   const battleNetClient = inject('battleNetClient') as BattleNetClient;
 
   async function mythicKeystoneProfileSeason(realm: string, character: string) {
     loading.value = true;
-    const response = await battleNetClient.fetchMythicKeystoneProfileSeason(
-      realm,
-      character,
-    );
-    loading.value = false;
-    return response;
+    error.value = null;
+
+    try {
+      const response = await battleNetClient.fetchMythicKeystoneProfileSeason(
+        realm,
+        character,
+      );
+      loading.value = false;
+      return response;
+    } catch (exception) {
+      loading.value = false;
+
+      if (exception instanceof Error) {
+        error.value = exception.message;
+        console.error('Fetch error:', exception.message);
+        throw error;
+      } else {
+        error.value = 'Unknown error occurred';
+        console.error(exception);
+      }
+    }
   }
 
-  return { mythicKeystoneProfileSeason, loading };
+  return { mythicKeystoneProfileSeason, loading, error };
 });
