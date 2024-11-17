59cd0315c708557ea74b70198634180e8fc0374c
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-17 23:53:42 +0100

    feat: add example environment variables and configure runtime settings for Battle.net API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import daisyui from "daisyui"
+import daisyui from 'daisyui';
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
@@ -8,7 +8,11 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   tailwindcss: {
     config: {
-      plugins: [daisyui]
-    }
-  }
-})
+      plugins: [daisyui],
+    },
+  },
+  runtimeConfig: {
+    battleNetClientId: process.env.BATTLE_NET_CLIENT_ID,
+    battleNetClientSecret: process.env.BATTLE_NET_CLIENT_SECRET,
+  },
+});
