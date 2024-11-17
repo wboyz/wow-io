7735094adaaf4cf25554a398257b2913ee14e75f
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-17 21:48:29 +0100

    feat: add configuration files for Prettier, ESLint, and EditorConfig; update Nuxt config and package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import daisyui from "daisyui"
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
+  tailwindcss: {
+    config: {
+      plugins: [daisyui]
+    }
+  }
 })
