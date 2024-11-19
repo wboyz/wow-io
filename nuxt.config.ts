b680ca737e21d3b0f46e1058f0d221c4e030e995
bertalanV <bertalan.verebelyi@gmail.com>
2024-11-19 23:12:14 +0100

    refactor: remove daisyUI plugin import from nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
-import daisyui from 'daisyui';
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
