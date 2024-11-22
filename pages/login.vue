fe4c3e2b3818a29b4d56c7fb4ab7888b02352e82
Csaba Verebélyi <csaba.verebelyi@gmail.com>
2024-11-22 22:26:39 +0100

    feat: :sparkles: update login page content and form submission method for enhanced user experience

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,16 @@
-<script setup lang="ts">
-const email = ref('');
-const password = ref('');
-</script>
-
 <template>
   <div class="hero bg-base-200 min-h-screen">
     <div class="hero-content flex-col lg:flex-row-reverse">
       <div class="text-center lg:text-left">
         <h1 class="text-5xl font-bold">Login now!</h1>
         <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
+          Welcome to our platform! Experience seamless and secure access to your
+          account. Join our community and explore the endless possibilities we
+          offer. Your journey to excellence starts here.
         </p>
       </div>
       <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form class="card-body">
+        <form class="card-body" method="post" action="/api/auth/login">
           <div class="form-control">
             <label class="label" for="email">
               <span class="label-text">Email</span>
@@ -25,8 +20,8 @@ const password = ref('');
               type="email"
               placeholder="email"
               class="input input-bordered"
+              name="email"
               required
-              v-model="email"
             />
           </div>
           <div class="form-control">
@@ -38,14 +33,9 @@ const password = ref('');
               type="password"
               placeholder="password"
               class="input input-bordered"
+              name="password"
               required
-              v-model="password"
             />
-            <label class="label">
-              <a href="#" class="label-text-alt link link-hover"
-                >Forgot password?</a
-              >
-            </label>
           </div>
           <div class="form-control mt-6">
             <button class="btn btn-primary">Login</button>
